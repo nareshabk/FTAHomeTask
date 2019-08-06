2e3eda17e380df766f684f9731031e891a447911
nareshabk <naresh.abk@gmail.com>
2019-08-06 20:32:59 +0530

    committ truly

---
+++ TrulyGreatNumber.java
@@ -0,0 +1,17 @@
+package com.naresh.ftaJavaCode;
+
+public class TrulyGreatNumber
+{
+    public static void main(String[] args) {
+        boolean GreatNumber =GreatNumber(12,6);
+        System.out.println(GreatNumber);
+
+    }
+    public static boolean GreatNumber(int a, int b) {
+        if(a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6)
+            return true;
+
+        return false;
+    }
+}
+
