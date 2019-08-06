53efef0a5b870016a395ab823f4bb6b9a0718dfe
nareshabk <naresh.abk@gmail.com>
2019-08-06 20:40:12 +0530

    committ forpyramid

---
+++ forpyramid.java
@@ -0,0 +1,23 @@
+package com.naresh.ftaJavaCode;
+
+public class forpyramid {
+    static int counter=0;
+    static int x=0;
+    public static void main(String[] args) {
+
+        pyramidfor(6,1);
+    }
+
+    public static void pyramidfor(int number,int x) {
+        for(int row=1;row<number;row++) {
+
+            for (int column = 1; column < row + 1; column++) {
+                System.out.print(counter + " ");
+                counter = counter + x;
+
+            }
+            System.out.println("");
+        }
+    }
+
+}
