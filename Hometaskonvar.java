0db2a3ec0e1ae05b6294b12a4af7a3aaba1b5277
nareshabk <naresh.abk@gmail.com>
2019-08-06 20:15:21 +0530

    committ hometaskovner

---
+++ Hometaskonvar.java
@@ -0,0 +1,18 @@
+package com.naresh.ftaJavaCode;
+
+
+public class Hometaskonvar {
+    public static void main(String[] args)
+    {
+        int total= countOfApples(3,2);
+
+        System.out.println("total number of apples" + " " + total);
+    }
+    public static int countOfApples(int Daniel,int Amber)
+    {
+        int Countofapples = Daniel + Amber;
+        return Countofapples;
+    }
+
+}
+
