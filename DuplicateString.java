3ce6d16e2af77768e96de73438e346c01ffd223b
nareshabk <naresh.abk@gmail.com>
2019-08-06 19:41:48 +0530

    committ caughtspeeding

--- DuplicateString.java
+++ DuplicateString.java
@@ -1,4 +1,21 @@
 package com.naresh.ftaJavaCode;
 
+
 public class DuplicateString {
+    public static void main(String[] args) {
+        String inputstring="HimaBindu";
+        int count=0;
+        for(int i=0;i<inputstring.length();i++)
+        {
+            for(int j=i+1;j<inputstring.length();j++)
+            {
+                if(inputstring.charAt(i)==inputstring.charAt(j))
+                {
+                    System.out.println(inputstring.charAt(j));
+                    count++;
+                }
+            }
+        }
+    }
 }
+
