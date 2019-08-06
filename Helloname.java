78d40ae00d0e71f289b0ecec6f44bf06bac98e62
nareshabk <naresh.abk@gmail.com>
2019-08-06 20:12:45 +0530

    committ hello

--- Helloname.java
+++ Helloname.java
@@ -1,4 +1,11 @@
 package com.naresh.ftaJavaCode;
 
-public class Helloname {
+public class Helloname
+{
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++)
+        {
+            System.out.println("Hello" +" "+args[i]);
+        } }
 }
+
