79ca15faa6949b362f75f8bb4bf9a642b11939c9
nareshabk <naresh.abk@gmail.com>
2019-08-06 19:04:47 +0530

    Commit pgm1

---
+++ pgm1.java
@@ -0,0 +1,7 @@
+package com.naresh.ftaJavaCode;
+
+public class pgm1 {
+    public static void main(String[] args) {
+        System.out.println("Hiii");
+    }
+}
--- src/main/java/com/naresh/ftaJavaCode/HelloWorld.java
+++
@@ -1,4 +0,0 @@
-package com.naresh.ftaJavaCode;
-
-public class HelloWorld {
-}
